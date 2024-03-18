438645d325785e0efe856a191fa53195fa5d9d98
matt <norrism7@msu.edu>
2024-03-18 11:31:29 -0400

    Initial Commit

---
+++ BlockVaultApp/BlockVaultAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockVaultAppApp.swift
+//  BlockVaultApp
+//
+//  Created by Matt Norris on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BlockVaultAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
